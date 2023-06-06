3c5dcccf1c237a61d57a53448dd31b9eba528885
Your Name <you@example.com>
2023-06-07 02:20:34 +0700

    update

--- bin/generate_language_scheme.dart
+++ bin/generate_language_scheme.dart
@@ -1,5 +1,6 @@
 import 'package:galaxeus_lib/galaxeus_lib.dart';
 import "package:path/path.dart" as path;
+import 'package:translate_client/translate_client.dart';
 import 'package:universal_io/io.dart';
 
 void main(List<String> args) async {
@@ -7,8 +8,12 @@ void main(List<String> args) async {
     language_raw: language_raw,
     default_language_code_id: "id",
     directory: Directory(path.join(Directory.current.path, "lib", "language")),
-    onData: (origin_data, language_code_id, default_language_code_id) async {
-      return null;
+    onData: (String origin_data, language_code_id, default_language_code_id) async {
+      try {
+        return await TranslateClient.translate(text: origin_data, toLanguageCode: language_code_id, fromLanguageCode: default_language_code_id);
+      } catch (e) {
+        return null;
+      }
     },
   );
 }
@@ -16,6 +21,6 @@ void main(List<String> args) async {
 Map language_raw = {
   "sign_in": {
     "phone_number_user": "Nomor Telepon Anda",
-    "phone_number_user_display": "Mohon Konfirmasi kode negara dan\nmasukan nomor telepon anda",
+    "phone_number_user_display": "Mohon Konfirmasi kode negara dan masukan nomor telepon anda",
   },
 };
